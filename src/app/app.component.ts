fdf73ae01b2abb56932e28baca36c70350fd608a
gabrielrede <gabrielredezx@gmail.com>
2024-02-07 17:07:55 -0300

    busca pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <app-card></app-card>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'C10';
